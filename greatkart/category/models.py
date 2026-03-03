3e02b8bf21105cd7fed6215b0bb9467b737d0f83
higoldi31 <goldiikumari71@gmail.com>
2026-03-04 01:32:49 +0530

    fix category name display in admin path

--- greatkart/category/models.py
+++ greatkart/category/models.py
@@ -12,5 +12,5 @@ class Category(models.Model):
         verbose_name='category'
         verbose_name_plural='categories'
 
-def __str__(self):
-    return self.category_name
+    def __str__(self):
+        return self.category_name
