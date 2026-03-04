8ccac9d032a4aff1f7c32bea96d0adcd533123f4
higoldi31 <goldiikumari71@gmail.com>
2026-03-04 15:45:22 +0530

    make store template

--- greatkart/store/views.py
+++ greatkart/store/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def store(request):
+    product=Product.objects.all().filter(is_available=True)
+    context={
+        'product': product,
+        'product_count': product.count(),
+    }
+    return render(request, 'store/store.html', context)
