6cc2976974519d01e339b82b83719ace3fefde01
higoldi31 <goldiikumari71@gmail.com>
2026-03-04 21:14:58 +0530

    troubleshoot product rendering

--- greatkart/store/views.py
+++ greatkart/store/views.py
@@ -1,11 +1,13 @@
+
+
 from django.shortcuts import render
 from .models import Product
 
 # Create your views here.
 def store(request):
-    product=Product.objects.all().filter(is_available=True)
-    context={
-        'product': product,
-        'product_count': product.count(),
+    products = Product.objects.filter(is_available=True)
+    context = {
+        'products': products,
+        'product_count': products.count(),
     }
     return render(request, 'store/store.html', context)
