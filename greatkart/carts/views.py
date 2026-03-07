f988d6c4407ca3323fd6d8fb0642bfc66b160bc0
higoldi31 <goldiikumari71@gmail.com>
2026-03-07 23:10:09 +0530

    fix cart counter on navbar

--- greatkart/carts/views.py
+++ greatkart/carts/views.py
@@ -33,12 +33,12 @@ def add_cart(request, product_id):
 
     return redirect('cart')
 
-def remove_cart(request, product_id):
+def remove_cart(request, product_id): 
     cart= Cart.objects.get(cart_id=_cart_id(request))
     product=get_object_or_404(Product, id=product_id)
     cart_item=CartItem.objects.get(product=product, cart=cart)
     if cart_item.quantity>1:
-        cart_item.quantity-=1
+        cart_item.quantity -= 1
         cart_item.save()
     else:
         cart_item.delete()
--- greatkart/store/views.py
+++ greatkart/store/views.py
@@ -1,6 +1,11 @@
 
+
+
 from category.models import Category
 from django.shortcuts import get_object_or_404, render
+
+from carts.models import CartItem
+from carts.views import _cart_id
 from .models import Product
 
 # Create your views here.
@@ -23,9 +28,12 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+       
     except Exception as e:
         raise e
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)    
