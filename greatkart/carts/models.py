2aabfd9e39e81230ff8346bc7606bc848d197a06
higoldi31 <goldiikumari71@gmail.com>
2026-03-07 12:03:10 +0530

    add to cart logic

--- greatkart/carts/models.py
+++ greatkart/carts/models.py
@@ -12,11 +12,12 @@ class Cart(models.Model):
         return self.cart_id
     
 class CartItem(models.Model):
-    product = models.ForeignKey('store.Product', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
-    
+    def sub_total(self):
+        return self.product.price * self.quantity
     def __str__(self):
-        return self.product.product_name
+        return self.product
